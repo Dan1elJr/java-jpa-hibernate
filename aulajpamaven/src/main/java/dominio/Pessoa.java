af618327297946556b7b29d15127cf395fcdb7d0
Daniel <danieljunior1903@gmail.com>
2023-09-21 14:00:54 -0400

    atualização

--- aulajpamaven/src/main/java/dominio/Pessoa.java
+++ aulajpamaven/src/main/java/dominio/Pessoa.java
@@ -2,10 +2,17 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
 	private String nome;
 	private String email;
